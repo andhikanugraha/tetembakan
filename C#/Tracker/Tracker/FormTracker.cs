16dbfefd71f33588cf22189e3eb565c97f4cd1b5
Muhammad Reza Mandala Putra <13509003@std.stei.itb.ac.id>
2013-03-23 16:39:29 +0700

    tracker
    commit tracker csharp (pembuka jalan kodingan)

---
+++ C#/Tracker/Tracker/FormTracker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tracker
+{
+    public partial class FormTracker : Form
+    {
+        public FormTracker()
+        {
+            InitializeComponent();
+        }
+    }
+}

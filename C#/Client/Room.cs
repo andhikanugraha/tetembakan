27fe8f6bb811fb9602c1a8989c29f9a86bdefd8e
Andhika Nugraha <andhika.nugraha@gmail.com>
2013-03-24 10:07:54 +0700

    Update Room

--- C#/Client/Room.cs
+++ C#/Client/Room.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -8,8 +10,27 @@ namespace Client
 {
     class Room
     {
-        public Peer Creator { get; set; }
-        public string Title { get; set; }
+        public Int32 ID { get; set; }
+        public Int32 PeerID { get; set; }
         public List<Peer> Peers { get; set; }
+    }
+
+    class RoomViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Room> Rooms { get; set; }
+
+        public RoomViewModel()
+        {
+            Rooms = new ObservableCollection<Room>();
+        }
+
+        public void PopulateRooms(IEnumerable<Room> rooms)
+        {
+            Rooms.Clear();
+            foreach (var room in rooms)
+            {
+                Rooms.Add(room);
+            }
+        }
     }
 }

3d2efdd3ff592c66111915e1c7f87fb742718988
Danny Andrianto <danny_andrianto@hotmail.com>
2013-03-24 11:38:23 +0700

    ini client server

---
+++ Sister1/Server.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Sister1
+{
+    class Server
+    {
+        public AsyncCallback pfnWorkerCallBack;
+        public Socket m_socListener;
+        public Socket m_socWorker;
+
+        static void Main(string[] args)
+        {
+            Server s = new Server();
+
+            try
+            {
+                //create the listening socket...
+                s.m_socListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPEndPoint ipLocal = new IPEndPoint(IPAddress.Any, 8221);
+                //bind to local IP Address...
+                s.m_socListener.Bind(ipLocal);
+                //start listening...
+                s.m_socListener.Listen(4);
+                // create the call back for any client connections...
+                Thread.Sleep(1);
+                s.m_socListener.BeginAccept(new AsyncCallback(s.OnClientConnect), null);
+                
+            }
+            catch (SocketException se) 
+            {
+                Console.WriteLine(se.Message);
+            }
+            while (1 == 1) ;
+        }
+
+        public void OnClientConnect(IAsyncResult asyn)
+        {
+            try
+            {
+                m_socWorker = m_socListener.EndAccept(asyn);
+                Console.WriteLine("Client connected");
+                WaitForData(m_socWorker);
+            }
+            catch (ObjectDisposedException)
+            {
+                System.Diagnostics.Debugger.Log(0, "1", "\n OnClientConnection: Socket has been closed\n");
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine(se.Message);
+            }
+
+        }
+
+
+
+        public class CSocketPacket
+        {
+            public System.Net.Sockets.Socket thisSocket;
+            public byte[] dataBuffer = new byte[500];
+        }
+
+        public void WaitForData(System.Net.Sockets.Socket soc)
+        {
+            try
+            {
+                if (pfnWorkerCallBack == null)
+                {
+                    pfnWorkerCallBack = new AsyncCallback(OnDataReceived);
+                }
+                CSocketPacket theSocPkt = new CSocketPacket();
+                theSocPkt.thisSocket = soc;
+                // now start to listen for any data...
+                soc.BeginReceive(theSocPkt.dataBuffer, 0, theSocPkt.dataBuffer.Length, SocketFlags.None, pfnWorkerCallBack, theSocPkt);
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine(se.Message);
+            }
+
+        }
+
+        public void OnDataReceived(IAsyncResult asyn)
+        {
+            try
+            {
+                CSocketPacket theSockId = (CSocketPacket)asyn.AsyncState;
+                //end receive...
+                int iRx = 0;
+                iRx = theSockId.thisSocket.EndReceive(asyn);
+                char[] chars = new char[iRx + 1];
+                System.Text.Decoder d = System.Text.Encoding.UTF8.GetDecoder();
+                int charLen = d.GetChars(theSockId.dataBuffer, 0, iRx, chars, 0);
+                System.String szData = new System.String(chars);
+                if (szData != "\0")
+                {
+                    Console.WriteLine(szData);
+                }
+                WaitForData(m_socWorker);
+            }
+            catch (ObjectDisposedException)
+            {
+                System.Diagnostics.Debugger.Log(0, "1", "\nOnDataReceived: Socket has been closed\n");
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine(se.Message);
+            }
+        }
+    }
+}

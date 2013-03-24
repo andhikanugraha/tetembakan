66351e8c9dedcc1be28ead86db8e2c97aa94cc9a
Andhika Nugraha <andhika.nugraha@gmail.com>
2013-03-24 09:35:57 +0700

    Added Peer and Room models

---
+++ C#/Client/Peer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Client
+{
+    class Peer
+    {
+        public string Name { public get; private set; }
+        public string ID { public get; private set; }
+
+        Peer(string Name, string ID)
+        {
+            this.Name = Name;
+            this.ID = ID;
+        }
+    }
+}
---
+++ C#/Client/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Client
+{
+    class Room
+    {
+        public Peer Creator { get; set; }
+        public string Title { get; set; }
+        public List<Peer> Peers { get; set; }
+    }
+}
